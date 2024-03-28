cd8ccc3660a5fb35dd56046aeb5171fad5d6f7d1
Fa19959 <fa6amyalmamari@gmail.com>
2024-03-29 02:46:54 +0400

    Added countries.geojson

--- src/componen/MyMap.jsx
+++ src/componen/MyMap.jsx
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 class MyMap extends Component {
     state = {  } 
     render() { 
-        return (<div><h1>My Map</h1></div>);
+        return( 
+        <div>
+            <h1 style={{ textAlign:"center" }}>My Map</h1>
+            </div>
+            );
     }
 }
  
